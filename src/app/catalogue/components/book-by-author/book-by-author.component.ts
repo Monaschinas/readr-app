6976e3f9a29d77a6303ff80485cf5cd6bc2dd600
Salomon Zegarra <salozm34@gmail.com>
2023-06-25 18:35:35 -0500

    feat: added OnInit method to book by author component.

--- src/app/catalogue/components/book-by-author/book-by-author.component.ts
+++ src/app/catalogue/components/book-by-author/book-by-author.component.ts
@@ -1,10 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../../../publish/models/book";
+import {BooksService} from "../../../publish/services/books.service";
+
 
 @Component({
   selector: 'app-book-by-author',
   templateUrl: './book-by-author.component.html',
   styleUrls: ['./book-by-author.component.css']
 })
-export class BookByAuthorComponent{
+export class BookByAuthorComponent implements OnInit{
+  books: Array<Book> =[];
+
+  constructor(
+    private booksService: BooksService,
+  ) {}
+
+  ngOnInit() {
+    this.booksService.getAll()
+      .subscribe(response => this.books = response);
+  }
 
 }
